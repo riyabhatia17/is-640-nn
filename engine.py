4e47f7ed28d3292064d5889d60f80eec2708cc32
riyabhatia17 <riya.bhatia01@student.csulb.edu>
2024-11-07 00:34:22 -0800

    Update engine.py

--- engine.py
+++ engine.py
@@ -65,7 +65,7 @@ class Value:
                 topo.append(v)
         build_topo(self)
 
-        # go one variable at a time and apply the chain rule to get its gradient
+        
         self.grad = 1
         for v in reversed(topo):
             v._backward()
